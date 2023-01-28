128bdea72cdd9f8680f92fc0089a7a7cbd8dd9bf
MikeyDN <Mikeydn24@gmail.com>
2023-01-28 23:13:28 +0200

    back to drf-yasg2

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import status
-from drf_yasg2.utils import swagger_auto_schema
+from drf_yasg.utils import swagger_auto_schema
 
 
 class OrderListView(APIView):
